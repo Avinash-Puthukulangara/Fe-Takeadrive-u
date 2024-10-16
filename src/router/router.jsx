f44f0ec26c48c8f37c3b86b7fbc8dbcc47109eb6
Avinash P <aviputhukulangara@gmail.com>
2024-10-16 17:14:19 +0530

    initial commit

---
+++ src/router/router.jsx
@@ -0,0 +1,24 @@
+import { createBrowserRouter } from "react-router-dom";
+import { Userlayout } from "../layout/Userlayout";
+import { Errorpage } from "../pages/user/Errorpage";
+import { Signuppage } from "../pages/user/Signuppage";
+import { Homepage } from "../pages/user/Homepage";
+
+export const router = createBrowserRouter([
+    {
+      path: "/",
+      element: <Userlayout />,
+      errorElement: <Errorpage />,
+      children: [
+        {
+            path: "",
+            element: <Signuppage />
+        },
+        {
+            path: "home",
+            element: <Homepage />
+        }
+      ],
+    },
+    
+  ]);
