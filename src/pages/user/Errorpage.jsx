37c19dd3f521fc735ff8afafef25a8d026e3fcd3
Avinash P <aviputhukulangara@gmail.com>
2024-10-17 21:37:14 +0530

    second commit

--- src/pages/user/Errorpage.jsx
+++ src/pages/user/Errorpage.jsx
@@ -1,7 +1,20 @@
+import { Button } from '@chakra-ui/react'
 import React from 'react'
+import { useNavigate } from 'react-router-dom'
 
 export const Errorpage = () => {
+
+  const navigate = useNavigate()
+
+  const handleClick = ()=> {
+    navigate('/home')
+  }
+
   return (
-    <div>Error Page</div>
+    <div className='flex flex-col items-center justify-center h-screen'>
+      <h1 className='text-pink-700 text-5xl font-semibold font-serif'>Oops!</h1>
+      <p className='text-slate-900 text-md font-bold'>Page Not Found.</p>
+      <Button colorScheme='orange' variant='solid' size='sm' onClick={(handleClick)}>Go to Home</Button>
+    </div>
   )
 }
