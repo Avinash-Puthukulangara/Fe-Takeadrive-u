5f234b27a84e445ea82c95a3ac02cf60446249e5
Avinash P <aviputhukulangara@gmail.com>
2024-10-27 21:19:32 +0530

    edis

--- src/config/axiosInstance.jsx
+++ src/config/axiosInstance.jsx
@@ -4,6 +4,6 @@ import React from "react";
 const API_URL = import.meta.env.VITE_API_URL;
 
 export const axiosInstance = axios.create({
-    baseURL: "be-takeadrive-production.up.railway.app/api",
+    baseURL: `${API_URL}/api`,
     withCredentials: true
 })
